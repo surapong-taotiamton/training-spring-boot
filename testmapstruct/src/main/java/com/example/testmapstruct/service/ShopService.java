4d8c0eecbf0535ae10b456924642e3172071da04
surapong <surapong.taotiamton@gmail.com>
2024-07-20 12:15:09 +0700

    Add spec for map struct

--- testmapstruct/src/main/java/com/example/testmapstruct/service/ShopService.java
+++ testmapstruct/src/main/java/com/example/testmapstruct/service/ShopService.java
@@ -1,7 +1,11 @@
 package com.example.testmapstruct.service;
 
+import com.example.testmapstruct.service.spec.ShopServiceSpec;
+
 public interface ShopService {
 
-
+    ShopServiceSpec.ShopInfo getShop(String shopId);
+    ShopServiceSpec.GoodsInfo getGoodsInfo(String goodsId);
+    ShopServiceSpec.GoodsFullInfo getGoodsFullInfo(String goodsId);
 
 }
--- testmapstruct/src/main/java/com/example/testmapstruct/service/spec/ShopServiceSpec.java
+++ testmapstruct/src/main/java/com/example/testmapstruct/service/spec/ShopServiceSpec.java
@@ -1,9 +1,14 @@
 package com.example.testmapstruct.service.spec;
 
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.math.BigDecimal;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Accessors(chain = true)
 @Data
 public class ShopServiceSpec {
@@ -14,21 +19,32 @@ public class ShopServiceSpec {
     public static class ShopInfo {
         private String shopId;
         private String shopName;
-        private String owner;
+        private String shopAddress;
+        private String shopEmail;
+        private String shopContactNo;
+        private String ownerFirstname;
+        private String ownerLastname;
     }
 
 
     @Accessors(chain = true)
     @Data
     public static class GoodsInfo {
-
+        private String goodsId;
+        private String goodsName;
+        private BigDecimal price;
     }
 
 
     @Accessors(chain = true)
     @Data
     public static class GoodsFullInfo {
-
+        private String goodsId;
+        private String goodsName;
+        private BigDecimal price;
+        private String shopId;
+        private String shopName;
+        private String shopContactNo;
     }
 
 
