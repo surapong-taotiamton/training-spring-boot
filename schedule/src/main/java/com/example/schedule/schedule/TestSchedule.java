ae4fb841e5e029312c6018c5db794881af555557
surapong <surapong.taotiamton@gmail.com>
2025-01-18 12:50:31 +0700

    Finish fix by properties

--- schedule/src/main/java/com/example/schedule/schedule/TestSchedule.java
+++ schedule/src/main/java/com/example/schedule/schedule/TestSchedule.java
@@ -2,17 +2,19 @@ package com.example.schedule.schedule;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
+@ConditionalOnProperty(name = "do.schedule", havingValue = "true", matchIfMissing = false)
 @Component
 public class TestSchedule {
 
     private static final Logger logger = LoggerFactory.getLogger(TestSchedule.class);
 
-    //@Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void testSchedule() throws  Exception{
         logger.info("Begin testSchedule");
 
